698a5ca6562e948503b1ec5edac24a5536f7e450
Michael Stucki <stcuthbertus@gmail.com>
2025-06-20 12:17:55 -0600

    complete oop exercise 03

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -3,7 +3,12 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Why does the output print in the order it does?
  *
- * You answer:
+ * You answer: because as a rule:
+ * 1. when a subclass constructor is invoked, the first command executed in it is an invocation of its superclass constructor
+ *    and this occurs automatically when no constructors are defined.
+ * 2. so, since C_1 extends B_1, when new C_1() is run, the C_1 default constructor first calls B_1's default constructor.
+ * 3. but since B_1 extends A_1, this results in the invocation of A_1's default constructor.
+ * 4. therefore, the order of constructor calls (as shown by the messages): is A_1, then B_1, then C_1.
  *
  */
 
