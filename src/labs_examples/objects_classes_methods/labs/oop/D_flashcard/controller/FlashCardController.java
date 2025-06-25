7b3a5d974883d661314e33025a14f752ffd57fbd
Michael Stucki <stcuthbertus@gmail.com>
2025-06-25 15:33:00 -0600

    move UI access from main to FlashcardView where it belows

--- src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/controller/FlashCardController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/controller/FlashCardController.java
@@ -1,22 +1,18 @@
 package labs_examples.objects_classes_methods.labs.oop.D_flashcard.controller;
 
+import labs_examples.objects_classes_methods.labs.oop.D_flashcard.ui.FlashCardView;
 import labs_examples.objects_classes_methods.labs.oop.D_flashcard.ui.IUserIOConsole;
 import labs_examples.objects_classes_methods.labs.oop.D_flashcard.ui.UserIO;
 
 public class FlashCardController {
+    private FlashCardView view = new FlashCardView();
     private UserIO io = new IUserIOConsole();
 
     public void run() {
         boolean keepGoing = true;
         int menuSelection = 0;
         while(keepGoing) {
-            io.print("Main Menu");
-            io.print("1. List Cards");
-            io.print("2. Add New Card");
-            io.print("3. Remove Card");
-            io.print("4. Exit");
-
-            menuSelection = io.readInt("Please enter a selection", 1, 4);
+            menuSelection = getMenuSelection();
 
             switch (menuSelection) {
                 case 1:
@@ -37,5 +33,9 @@ public class FlashCardController {
         }
         io.print("GOOD BYE!");
         io.close();
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
     }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/ui/FlashCardView.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/ui/FlashCardView.java
@@ -1,4 +1,16 @@
 package labs_examples.objects_classes_methods.labs.oop.D_flashcard.ui;
 
 public class FlashCardView {
+
+    private UserIO io = new IUserIOConsole();
+
+    public int printMenuAndGetSelection() {
+        io.print("Main Menu");
+        io.print("1. List Cards");
+        io.print("2. Add New Card");
+        io.print("3. Remove Card");
+        io.print("4. Exit");
+
+        return io.readInt("Please enter a selection", 1, 4);
+    }
 }
