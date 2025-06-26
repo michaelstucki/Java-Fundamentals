ba8cfccf4c4be9380f2ef94ef5d909c76bf1cec2
Michael Stucki <stcuthbertus@gmail.com>
2025-06-25 22:10:37 -0600

    fix tab in main

--- src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/FlashCardApp.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_flashcard/FlashCardApp.java
@@ -11,7 +11,7 @@ public class FlashCardApp {
 
     public static void main(String[] args) {
         UserIO io = new IUserIOConsole();                                       // console implementation of the UserIO interface
-    FlashCardView view = new FlashCardView(io);                                 // dependency inject io into viewer
+        FlashCardView view = new FlashCardView(io);                             // dependency inject io into viewer
         FlashCardDao dao = new IFlashCardDaoFile();                             // file implementation of the DAO
         FlashCardController controller = new FlashCardController(view, dao);    // dependency inject view, dao into controller
         controller.run();                                                       // kickoff app
