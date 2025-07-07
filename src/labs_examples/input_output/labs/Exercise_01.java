56b6b0478c05042f9d81efec1aa6b9675447fe61
Michael Stucki <stcuthbertus@gmail.com>
2025-07-07 07:14:45 -0600

    input_output labs - Exercise 01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,15 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
-
-
+        String fileInPath = "src/labs_examples/input_output/files/char_data.txt";
+        String fileOutPath = "src/labs_examples/input_output/files/exercise01.txt";
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileInPath));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileOutPath))) {
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+            while ((bytesRead = bis.read(buffer)) != -1) bos.write(buffer);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
