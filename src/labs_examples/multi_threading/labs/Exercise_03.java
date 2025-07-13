533fef6620ed9c97da7bc4aecb2cfd9ebc866765
Michael Stucki <stcuthbertus@gmail.com>
2025-07-12 21:31:45 -0600

    multi threading exercise 03

--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -5,3 +5,20 @@ package labs_examples.multi_threading.labs;
  *
  *      In one of the previous exercises, demonstrate changing the priority of a thread
  */
+
+class Exercise_03 implements Runnable {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Exercise_03(), "01");
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+
+        Thread t2 = new Thread(new Exercise_03(), "02");
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello, Exercise_03 " + Thread.currentThread().getName());
+    }
+}
