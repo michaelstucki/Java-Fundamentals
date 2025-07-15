028f02be7ca1cbe05ffc2cb9da8a96f8909a1e3c
Michael Stucki <stcuthbertus@gmail.com>
2025-07-14 18:46:45 -0600

    multi threading lab Exercise 05

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -5,4 +5,37 @@ package labs_examples.multi_threading.labs;
  *
  *      Demonstrate the use of a wait() and notify()
  */
+public class Exercise_05 implements Runnable {
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 3; i++) {
+                if (Thread.currentThread().getName().equalsIgnoreCase("hello")) {
+                    notify();
+                    System.out.print("Hello! ");
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    notify();
+                    System.out.print("Goodbye! ");
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            notify();
+        }
+    }
 
+    public static void main(String[] args) {
+        Exercise_05 r = new Exercise_05();
+        Thread t1 = new Thread(r, "hello");
+        Thread t2 = new Thread(r, "goodbye");
+        t1.start();
+        t2.start();
+    }
+}
