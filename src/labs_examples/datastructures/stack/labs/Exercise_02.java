1994fb32496cbe4facd1b4678a6b43c21b7f9923
Michael Stucki <stcuthbertus@gmail.com>
2025-08-31 09:30:23 -0600

    stack array Ex. 02

--- src/labs_examples/datastructures/stack/labs/Exercise_02.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_02.java
@@ -1,6 +1,6 @@
 package labs_examples.datastructures.stack.labs;
 
-/**
+/*
  *      Stacks - Exercise_02
  *
  *      Write a new custom (Generic) Stack class that uses a traditional Array as the underlying data structure
@@ -18,3 +18,102 @@ package labs_examples.datastructures.stack.labs;
  *      TIP: To initialize a new array of a Generic type you can use this:
  *           T[] data = (T[]) new Object[10];
  */
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+        MyStack<String> ms = new MyStack<>();
+        System.out.println(ms.size());
+        ms.print();
+        ms.push("A");
+        ms.push("B");
+        ms.push("C");
+        ms.push("D");
+        System.out.println(ms.size());
+        ms.print();
+        System.out.println(ms.peekView());
+        System.out.println(ms.peekLast());
+        System.out.println(ms.pop());
+        System.out.println(ms.size());
+        System.out.println(ms.pop());
+        System.out.println(ms.size());
+        System.out.println(ms.pop());
+        System.out.println(ms.size());
+        System.out.println(ms.pop());
+        System.out.println(ms.size());
+        ms.print();
+        System.out.println(ms.peekView());
+        System.out.println(ms.peekLast());
+    }
+}
+
+class MyStack<T> {
+    @SuppressWarnings("unchecked")
+    private T[] data = (T[]) new Object[4];
+    private int size = data.length;
+    private int cursor = 0;
+
+    public void push(T element) {
+        data[cursor++] = element;
+        if ((double) cursor / size >= 0.75) resize(size * 2);
+    }
+
+    public T pop() {
+        T tmp = null;
+        try {
+            if (cursor == 0) throw new StackArrayException("empty array");
+            tmp = data[--cursor];
+            data[cursor] = null;
+            if ((double) cursor / size <= 0.25) resize(size / 2);
+        } catch (StackArrayException e) {
+            System.out.println(e.getMessage());
+        }
+        return tmp;
+    }
+
+    public void resize(int newSize) {
+        @SuppressWarnings("unchecked")
+        T[] tmp = (T[]) new Object[newSize];
+        if (newSize > size) {
+            if (size >= 0) System.arraycopy(data, 0, tmp, 0, size);
+        } else {
+            System.arraycopy(data, 0, tmp, 0, newSize);
+        }
+        data = tmp;
+        size = newSize;
+    }
+
+    public T peekView() {
+        T tmp = null;
+        try {
+            if (cursor == 0) throw new StackArrayException("empty stack");
+            tmp = data[cursor - 1];
+        } catch (StackArrayException e) {
+            System.out.println(e.getMessage());
+        }
+        return tmp;
+    }
+
+    public T peekLast() {
+        T tmp = null;
+        try {
+            if (cursor == 0) throw new StackArrayException("empty stack");
+            tmp = data[0];
+        } catch (StackArrayException e) {
+            System.out.println(e.getMessage());
+        }
+        return tmp;
+    }
+
+    public void print() {
+        for(int i = 0; i < cursor; i++) System.out.print(data[i] + " ");
+        System.out.println();
+    }
+
+    public int size() { return cursor; }
+}
+
+class StackArrayException extends Exception {
+    public StackArrayException(String message) {
+        super(message);
+    }
+}
